d050f75096a749a29c4861b7bcda0d884c87bf49
krishnapriya-venkatakrishnan <krishnapriyavenkatakrishnan@gmail.com>
2025-04-01 18:35:27 +0200

    Install @clerk/nextjs, @clerk/themes; Implement ClerkProvider in app/layout.tsx; Implement Signin button in the respective document page; Setup sign-in and sign-up page

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 
 const fontSans = FontSans({
   variable: "--font-geist-mono",
@@ -19,6 +21,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <ClerkProvider appearance={{
+      baseTheme: dark,
+      variables: {
+        colorPrimary: "#3371FF",
+        fontSize: "16px",
+      }
+    }}>
     <html lang="en" suppressHydrationWarning>
       <body
         className={cn("min-h-screen font-sans antialiased", fontSans.variable)}
@@ -26,5 +35,6 @@ export default function RootLayout({
         {children}
       </body>
     </html>
+    </ClerkProvider>
   );
 }
