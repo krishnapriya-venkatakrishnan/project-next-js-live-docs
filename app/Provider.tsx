517bfee2223858ceab8634cea297b3ae33d92207
krishnapriya-venkatakrishnan <krishnapriyavenkatakrishnan@gmail.com>
2025-04-03 15:28:42 +0200

    Create getDocumentUsers server action; Get the list of users available in the room.

--- app/Provider.tsx
+++ app/Provider.tsx
@@ -2,7 +2,8 @@
 "use client";
 
 import Loader from "@/components/Loader";
-import { getClerkUsers } from "@/lib/actions/user.actions";
+import { getClerkUsers, getDocumentUsers } from "@/lib/actions/user.actions";
+import { useUser } from "@clerk/nextjs";
 import {
   LiveblocksProvider,
   ClientSideSuspense,
@@ -13,12 +14,24 @@ import { ReactNode } from "react";
 // also remove the publicApiKey, because later we will use id tokens, a different way of authenticating our users.
 // we will soon implement this endpoint- /api/liveblocks-auth
 const Provider = ({children}: {children: ReactNode}) => {
+  
+  const { user: clerkUser } = useUser();
+  
   return (
     <LiveblocksProvider 
     authEndpoint="/api/liveblocks-auth"
     resolveUsers={async({userIds}) => {
       const users = await getClerkUsers({userIds});
       return users;
+    }}
+    // to know which users are available in the room.
+    resolveMentionSuggestions={async({text, roomId}) => {
+      const roomUsers = await getDocumentUsers({
+        roomId, 
+        currentUser: clerkUser?.emailAddresses[0].emailAddress,
+        text,
+      });
+      return roomUsers;
     }}
     >
       <ClientSideSuspense fallback={<Loader />}>
