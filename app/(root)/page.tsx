abba9db3b4029784ab535e715bd1c37994396a37
krishnapriya-venkatakrishnan <krishnapriyavenkatakrishnan@gmail.com>
2025-04-16 16:51:09 +0200

    Style home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -30,7 +30,7 @@ const Home = async () => {
 
       {
         roomDocuments.data.length > 0 
-        ? (<div className="document-list-container">
+        ? (<div className="document-list-container md:mt-30">
             <div className="document-list-title">
               <h3 className="text-28-semibold">
                 All documents
